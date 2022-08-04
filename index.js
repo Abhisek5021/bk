931357eb871acc909057078d8f6e4e0222882f9b
Abhisek5021 <abhisekpaul2000@.com>
2022-08-04 17:23:23 +0530

    Semi

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-let c,b,sum
-let card1=document.querySelector("#d")
-function f(){
-    b=generate()
-    c=generate()
-    card
-    1.textContent="Cards: "+c+ " "+b
-    g();
-}
-let sum1=document.querySelector("#e")
-function g(){
-    sum=c+b
-    sum1.textContent="Sum: "+sum;
-}
+let cards=[]
+let sum=0
 let message=document.querySelector("#message-el")
-function a()
-{
-    message.textContent="Hello World"
-    f();
+let suuum=document.querySelector("#e")
+let caards=document.querySelector("#d")
+function startgame(){
+    if(sum > 21){
+        message.textContent="You have lost the game"
+    }else if(sum < 21){
+        message.textContent="Do you want to draw a new card"
+    }else if(sum===21){
+        message.textContent="You have got the Blackjack"
+    }
+    card()
+    sumtake()
 }
-function generate(){
-    let Num=Math.floor(Math.random()*13)+1
-    return Num;
+function sumtake(){
+    sum = cards[0]+cards[1]
+    suuum.textContent="Sums:" + sum
+
+}
+function card(){
+    let d=generate()
+    let f=generate()
+    cards.push(d)
+    cards.push(f)
+    caards.textContent="Cards: " + cards[0] + " " + cards[1]
+    
+}
+function generate(){ 
+    let c= Math.floor(Math.random()*13)+1
+    if(c>=11){
+        return 11
+    }else if(c<11){
+        return c
+    }
 }
